ce7b27d0292538b54bc501251fb44bf174d7db96
y5-netizen <yishke5@gmail.com>
2026-06-15 16:17:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ﻿const PIECE_IMAGES = {
-    'P': 'images/wp.png', 'R': 'images/wr.png', 'N': 'images/wn.png', 'B': 'images/wb.png', 'Q': 'images/wq.png', 'K': 'images/wk.png',
-    'p': 'images/bp.png', 'r': 'images/br.png', 'n': 'images/bn.png', 'b': 'images/bb.png', 'q': 'images/bq.png', 'k': 'images/bk.png'
+    'P': 'wp.png', 'R': 'wr.png', 'N': 'wn.png', 'B': 'wb.png', 'Q': 'wq.png', 'K': 'wk.png',
+    'p': 'bp.png', 'r': 'br.png', 'n': 'bn.png', 'b': 'bb.png', 'q': '/bq.png', 'k': 'bk.png'
 };
 
 const PIECE_VALUES = {
