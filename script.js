108135298c294968c66554ac73ff144667ccea14
y5-netizen <yishke5@gmail.com>
2026-06-25 13:16:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('Chess build: 2026-06-24 rebuilt-final');
 const PIECE_IMAGES = {
-    'P': 'images/wp.png', 'R': 'images/wr.png', 'N': 'images/wn.png', 'B': 'images/wb.png', 'Q': 'images/wq.png', 'K': 'images/wk.png',
+    'P': /wp.png', 'R': 'images/wr.png', 'N': 'images/wn.png', 'B': 'images/wb.png', 'Q': 'images/wq.png', 'K': 'images/wk.png',
     'p': 'images/bp.png', 'r': 'images/br.png', 'n': 'images/bn.png', 'b': 'images/bb.png', 'q': 'images/bq.png', 'k': 'images/bk.png'
 };
 
